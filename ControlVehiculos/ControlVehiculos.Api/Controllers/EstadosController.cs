37847c00cf4c8cf829e7cccdae08d127088ccce1
Luis Avila <wichoxy@gmail.com>
2026-06-26 00:52:25 -0600

    feat: crear controlador api para catalogos utilizando cache singleton

---
+++ ControlVehiculos/ControlVehiculos.Api/Controllers/EstadosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ControlVehiculos.Api.Services;
+
+namespace ControlVehiculos.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // La URL global será: api/estados
+public class EstadosController : ControllerBase
+{
+    private readonly IEstadoCacheService _cacheService;
+
+    // Inyectamos nuestro Singleton de caché
+    public EstadosController(IEstadoCacheService cacheService)
+    {
+        _cacheService = cacheService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetEstados()
+    {
+        var estados = await _cacheService.ObtenerEstadosAsync();
+        return Ok(estados); // Retorna la lista de la caché en formato JSON
+    }
+}
